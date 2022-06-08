ae1467832c1ae305d376c1f77a18bac129b1a6bb
nauman415 <naumanqayyum1@gmail.com>
2022-06-08 16:55:33 +0500

    new test
    bhai check

--- WindowsProject1/WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -124,9 +124,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
    
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
+    
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
